cd094c2ad42eacd520af558147b818bb93f134e4
Parker Knight <parkerknight@Parkers-MacBook-Air.local>
2017-10-26 15:55:20 -0400

    Start work on s

--- src/bin/s.rs
+++ src/bin/s.rs
@@ -1,7 +1,33 @@
 extern crate clap;
-use clap::App;
+extern crate file;
+
+use clap::{Arg, App};
+use std::process::Command;
 
 fn main(){
-    println!("asdF");
+    let pwd = Command::new("pwd")
+        .output()
+        .expect("FAILED TO INSTALL");
+    let pwd = String::from_utf8_lossy(&pwd.stdout);
+    let pwd = pwd.trim().to_owned();
+    let s = App::new("s")
+        .version("v0.1")
+        .author("Parker K")
+        .about("Saves the current location")
+        .arg(Arg::with_name("NAME")
+            .help("Names the alias for this saved location")
+            .required(true)
+            .index(1)).
+        get_matches();
+    let save_name = s.value_of("NAME").unwrap().to_owned();
+    //io(&save_name).expect("failed to find file");
+    println!("{}", save_name);
 
+}
+//TODO: fix file pathing for writing to the saved locations text file
+fn io(name: &String) -> file::Result<()>{
+    let sgdloc = file::get_text("../sgd_location.txt")?;
+    let savedloc = sgdloc + "src/saved_locations.txt";
+    file::put(savedloc, name)?;
+    Ok(())
 }
--- src/install.rs
+++ src/install.rs
@@ -1,7 +1,29 @@
 extern crate clap;
+extern crate file;
+use std::convert::AsRef;
+use std::ffi::OsString;
 use std::process::Command;
 
 fn main() {
+    let pwd = Command::new("pwd")
+        .output()
+        .expect("FAILED TO INSTALL");
+    let pwd = String::from_utf8_lossy(&pwd.stdout);
+    let pwd = pwd.trim().to_owned();
+    //io(&pwd); 
 
+    Command::new("mv")
+        .arg("target/debug/s")
+        .arg("/usr/local/bin")
+        .spawn()
+        .expect("FAILED TO INSTALL");
     println!("Building...");
+    
+}
+
+
+fn io(wd: &String) -> file::Result<()>{
+    let wd = wd.to_owned();
+    file::put("src/sgd_location.txt", wd)?;
+    Ok(())
 }
